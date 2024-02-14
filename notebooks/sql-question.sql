5699dc4e0c3c387965b5af336dd54aeaa7227e01
Jamie Latham <jamielatham15@gmail.com>
2024-02-14 14:23:47 +0000

    add cleaned files

--- notebooks/sql-question.sql
+++ notebooks/sql-question.sql
@@ -1,7 +1,14 @@
 /*markdown
-# Question:
+# SQL Challenge 
 
-Using the 'sales_target', 'orders', and 'order_details' tables, write a SQL query to find the top 5 months where the sales team exceeded their target in any category in terms of percentage.
+Description:
+
+- You have data about an e-commerce site
+- The 'sales_target' table contains the monthly target for each product category
+- The 'orders' table contains records of each order
+- The 'order_details' table contains details about the order such as purchased items, amounts, and price.
+
+Run the queries below to see a sample of each dataset
 */
 
 SELECT * from sales_target LIMIT 10;
@@ -10,49 +17,33 @@ SELECT * from orders LIMIT 10;
 
 SELECT * from order_details LIMIT 10;
 
-SELECT
-    *,
-    EXTRACT(MONTH FROM TO_DATE("Month of Order Date", 'Mon-YY')) AS month,
-    EXTRACT(YEAR FROM TO_DATE("Month of Order Date", 'Mon-YY')) AS year
-FROM sales_target;
+/*markdown
+## Question 1
 
-with targets as (
-    SELECT
-        *,
-        EXTRACT(MONTH FROM TO_DATE("Month of Order Date", 'Mon-YY')) AS month,
-        EXTRACT(YEAR FROM TO_DATE("Month of Order Date", 'Mon-YY')) AS year
-    FROM sales_target
-),
+#### Using the 'sales_target', 'orders', and 'order_details' tables, write a SQL query to find the top 5 months where the sales team exceeded their target in any category in terms of percentage.
+*/
 
-customer_sales as (
+with sales_by_category as (
+    select
+        category,
+        month,
+        year,
+        sum(amount * quantity) as total_sales
+    from orders o
+    inner join order_details od on o.order_id = od.order_id
+    group by year, month, category
+)
+
 select
-    o."Order ID",
-    to_date("Order Date", 'DD-MM-YYYY'),
-    "CustomerName",
-    "Amount",
-    "Category",
-    EXTRACT(MONTH FROM TO_DATE("Order Date", 'DD-MM-YYYY')) AS month,
-    EXTRACT(YEAR FROM TO_DATE("Order Date", 'DD-MM-YYYY')) AS year
-from orders o
-inner join order_details od on o."Order ID" = od."Order ID"
-
-)
-
-, total_sales as (
-select 
-    year,
-    month,
-    "Category",
-    SUM("Amount") as total_sales
-from customer_sales co
-group by year, month, "Category"
-)
-
-select 
-*,
-ROUND(CAST((total_sales * 100) / "Target" - 100 as NUMERIC), 2) as perc_above_target
-from total_sales cs
-inner join targets t on cs.year = t.year and cs.month = t.month and cs."Category" = t."Category"
-where total_sales > "Target"
-order by ROUND(CAST((total_sales * 100) / "Target" as NUMERIC), 2) desc
+cs.year,
+cs.month, 
+ROUND(CAST((total_sales * 100) / target - 100 as NUMERIC), 2) as perc_above_target
+from sales_by_category cs
+inner join sales_target st on cs.year = st.year and cs.month = st.month
+where total_sales > target
+order by ROUND(CAST((total_sales * 100) / target as NUMERIC), 2) desc
 limit 5
+
+/*markdown
+
+*/
