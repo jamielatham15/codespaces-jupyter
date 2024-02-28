436a60bf464710b8d35d8147fa32de309ba642e8
Jamie Latham <jamielatham15@gmail.com>
2024-02-28 08:22:29 +0000

    finished

--- notebooks/sql-test.sql
+++ notebooks/sql-test.sql
@@ -17,3 +17,25 @@ SELECT * from order_details LIMIT 10;
 /*markdown
 #### Using the 'sales_target', 'orders', and 'order_details' tables, write a SQL query to find the top 5 months where the sales team exceeded their target in any category and show the percentage above target.
 */
+
+
+with yearmonth_sales as (select  o.year,o.month,od.category,sum (od.amount*quantity) as Total_sum from orders o join order_details od on o.order_id=od.order_id
+group by o.year,o.month,od.category,)
+sales_diff as(
+
+select year,month,category,(total_sum-target) as diff from sales_target st
+join yearmonth_sales ys
+on ys.year=st.year
+and ys.month=st.month
+and ys.Total_sum>st)
+
+select *,dense_rank
+
+.taret
+
+
+
+
+
+
+
