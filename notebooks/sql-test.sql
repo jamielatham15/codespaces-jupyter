015951968d9a2bb1e3f263876dde17f8935b97cc
Jamie Latham <jamielatham15@gmail.com>
2024-07-08 12:27:49 +0000

    start building new tests for data engineer

--- notebooks/sql-test.sql
+++ notebooks/sql-test.sql
@@ -15,5 +15,33 @@ SELECT * from orders LIMIT 10;
 SELECT * from order_details LIMIT 10;
 
 /*markdown
-#### Using the 'sales_target', 'orders', and 'order_details' tables, write a SQL query to find the top 5 months where the sales team exceeded their target in any category and show the percentage above target.
+### Write a query to find customers who have made consecutive purchases. For each such customer, provide their name, the order dates of the consecutive purchases, and the number of days between these orders.
 */
+
+"""
+customer_name	order_date	next_order_date	days_between
+Shikhar	        2019-01-04	2019-01-05	       1
+Shreya	        2019-01-17	2019-01-18	       1
+"""
+
+WITH consecutive_orders AS (
+    SELECT 
+        o.customer_name,
+        o.order_date::date AS order_date,
+        LEAD(o.order_date::date) OVER (PARTITION BY o.customer_name ORDER BY o.order_date::date) AS next_order_date
+    FROM 
+        orders o
+)
+SELECT 
+    customer_name,
+    order_date,
+    next_order_date,
+     next_order_date - order_date AS days_between
+FROM 
+    consecutive_orders
+WHERE 
+    next_order_date IS NOT NULL
+AND 
+    next_order_date - order_date = 1
+ORDER BY 
+    customer_name, order_date;
